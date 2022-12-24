a1b6b4c1cbe94c20661043e7427c55597f4a4391
AshleyThew <ashl.e.thew@gmail.com>
2022-12-25 09:47:45 +1300

    Remove unused code

--- src/main/java/com/wikidialogue/WikiDialoguePlugin.java
+++ src/main/java/com/wikidialogue/WikiDialoguePlugin.java
@@ -66,7 +66,7 @@ public class WikiDialoguePlugin extends Plugin {
         clientToolbar.removeNavigation(navButton);
         eventBus.unregister(wikiDialoguePanel);
         keyManager.unregisterKeyListener(shiftListener);
-        //overlayManager.remove(wikiDialogueOverlay);
+        
         log.info("Wiki Dialogue stopped!");
     }
 
